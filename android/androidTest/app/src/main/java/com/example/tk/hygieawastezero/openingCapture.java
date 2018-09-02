4e603c4ef44cd355e6417724cc9b44665d1339a4
tkallaus <tkallaus@asu.edu>
2018-09-01 20:57:11 -0700

    Added camera preview to opening activity

--- android/androidTest/app/src/main/java/com/example/tk/hygieawastezero/openingCapture.java
+++ android/androidTest/app/src/main/java/com/example/tk/hygieawastezero/openingCapture.java
@@ -1,12 +1,27 @@
 package com.example.tk.hygieawastezero;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class openingCapture extends AppCompatActivity {
+import java.io.IOException;
+
+public class openingCapture extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    Camera camera;
+
+    SurfaceHolder previewHolder;
+
+    final int CAMERA_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +37,56 @@ public class openingCapture extends AppCompatActivity {
                 startActivity(startPreview);
             }
         });
+
+        final SurfaceView camPreview = findViewById(R.id.cameraPreview);
+        previewHolder = camPreview.getHolder();
+        //For android 6 and above permissions
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }
+        else {
+            previewHolder.addCallback(this);
+            previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+
+        Camera.Parameters parameters;
+        parameters = camera.getParameters();
+
+        camera.setDisplayOrientation(90);
+        parameters.setPreviewFrameRate(30);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    previewHolder.addCallback(this);
+                    previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                } else {
+                    Toast.makeText(this, "Camera Permissions are required for this app", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
     }
 }
