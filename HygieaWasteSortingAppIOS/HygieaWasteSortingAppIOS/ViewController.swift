de212b9fbd4c9572560edf09a9d3e693b36dd4a2
Hesham <halgham2@asu.edu>
2018-09-02 20:56:52 -0700

    #57
    Create the app and the camera view

---
+++ HygieaWasteSortingAppIOS/HygieaWasteSortingAppIOS/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  HygieaWasteSortingAppIOS
+//  A lot of the code is based on many tutorials
+//  Created by Hesham Alghamdi on 9/1/18.
+//  Copyright © 2018 Arizona State University. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var captureSession = AVCaptureSession()
+    var backCamera : AVCaptureDevice?
+    var frontCamera : AVCaptureDevice?
+    var currentCamera : AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    var  image: UIImage?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCaptureSession()
+        setupDevice()
+        setupIO()
+        setupPreview()
+        startRunningCaptureSession()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupCaptureSession(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+    }
+    
+    func setupDevice(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back{
+                backCamera = device
+            }else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        currentCamera = backCamera
+        
+    }
+
+
+    func setupIO(){
+        do{
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera! )
+            captureSession.addInput(captureDeviceInput)
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format:[AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+            captureSession.addOutput(photoOutput!)
+        }
+        catch{
+            
+        }
+        
+    }
+    
+    func setupPreview(){
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        previewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(previewLayer!, at: 0)
+        
+    }
+    
+    func startRunningCaptureSession(){
+        captureSession.startRunning()
+    }
+    
+    
+    
+    @IBAction func CameraButtonAction(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+        //performSegue(withIdentifier: "DisplayPhotoSegue", sender: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DisplayPhotoSegue" {
+            let previewVC = segue.destination as! PreviewViewController
+            previewVC.image = self.image
+        }
+    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+}
+
+extension ViewController : AVCapturePhotoCaptureDelegate{
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation(){
+            image = UIImage(data: imageData)
+            performSegue(withIdentifier: "DisplayPhotoSegue" , sender: nil)
+    
+        }
+    }
+}
