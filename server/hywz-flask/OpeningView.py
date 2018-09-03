02c2cf1cb5da08a84845a393058d1af9353003a7
Joshua Albin <jaalbin@asu.edu>
2018-09-02 21:12:09 -0700

    #69 Opening View
    Create Button for app to take pitcure

---
+++ server/hywz-flask/OpeningView.py
@@ -0,0 +1,28 @@
+﻿#imports
+import QApplication
+import QWidget
+import QPushButton
+import pyqtSlot
+
+#class for task
+class OpeningView(QWidget):
+
+    #init func
+    def __init__(self):
+        super().__init__()
+        self.left = 5
+        self.top = 5
+        self.width = 100
+        self.height = 100
+        self.initUI()
+        
+    #name of app
+    def on_click(self):
+        print('Hygiea')
+        
+    #button push func
+    def button(self):
+        button = QPushButton('Take Picture')   
+        button.move(100, 30)     
+        button.clicked.connect(self.on_click)
+        
