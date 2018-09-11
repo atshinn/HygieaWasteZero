b1211d2448e26312120d6c466f790e4eaf2403fc
Joshua Albin <jaalbin@asu.edu>
2018-09-10 19:31:54 -0700

    Task #99 Store Credentials
    Beginning to store the credentials and check them.

---
+++ server/hywz-flask/CheckCredentials.py
@@ -0,0 +1,3 @@
+﻿#imports
+import json
+
---
+++ server/hywz-flask/StoreCredentials.py
@@ -0,0 +1,30 @@
+﻿#imports
+import json
+
+#read ins
+#NEED TO READ IN FROM APP???
+username = ''
+password = ''
+
+#----------DATA FILE----------
+
+#creates data
+data = {}
+
+#creates file info
+data['login'] = []
+
+#----------ADDING IN CREDENTIALS----------
+
+#creates new user
+data['login'].append({
+    'username': username,
+    'password': password
+})
+
+#----------ADD TO FILE----------
+
+#dumps data to file
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
+    
