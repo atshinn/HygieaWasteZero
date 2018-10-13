1eb2c873fff49fc47957395e0bce4bc6f6a1e94d
Hesham <halgham2@asu.edu>
2018-10-13 03:54:01 -0700

    task #126
    A mockup for to get a token back from AWS it's still got a couple of syntax errors but the logic seems fine for now

--- login/login/SignUpViewController.swift
+++ login/login/SignUpViewController.swift
@@ -1,161 +1,161 @@
-
-import foundation
-import UIKit
-
-class SignUpViewController: UIViewController {
-    
-    //String Inputs
-    var name: String = ""
-    var username: String = ""
-    var email: String = ""
-    var password: String = ""
-    var repeatPassword: String = ""
-    
-    //Array
-    let symbols = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm0123456789-_`. "
-    
-    //Used for outputing an error statement for user
-    @IBOutlet var ErrorStatement: UILabel!
-    
-    //Name input
-    @IBOutlet var takeName: UITextField!
-    
-    //Username input
-    @IBOutlet var takeUsername: UITextField!
-    
-    //Email input
-    @IBOutlet var takeEmail: UITextField!
-    
-    //Password and Password check input
-    @IBOutlet var takePassword: UITextField!
-    
-    @IBOutlet var takeRepeatPassword: UITextField!
-    
-    //Check to make sure the inputs are correct
-    @IBAction func Register(sender: ViewController) {
-        
-        //Take Inputs
-        name = takeName.text!
-        username = takeUsername.text!
-        email = takeEmail.text!
-        password = takePassword.text!
-        repeatPassword = takeRepeatPassword.text!
-        
-        //Flag to see if username and password can be accepted
-        var flag: Bool = true
-        
-        //Check if passwords match
-        if password != repeatPassword {
-            flag = false
-            ErrorStatement.text = "Passwords do not match"
-        }
-        
-        //Checks length of username
-        if username.count < 8 {
-            if username.count > 16 {
-                flag = false
-                ErrorStatement.text = "Username must be between 8 and 16 characters"
-            }
-        }
-        
-        //Checks length of password
-        if password.count < 8 {
-            if password.count > 16 {
-                flag = false
-                ErrorStatement.text = "Password must be between 8 and 16 characters"
-            }
-        }
-        
-        //Checks that username is the correct symbols
-        for i in username {
-            
-            for j in symbols {
-                if i == j {
-                    break
-                }
-                else {
-                    flag = false
-                    ErrorStatement.text = "Username containing incorrect symbols. Please only use Letters, Numbers, Spaces, and the following symbols: '-', '_', '`, '."
-                }
-            }
-        }
-        
-        //Checks that password is the correct symbols
-        for i in password {
-            
-            for j in symbols {
-                if i == j {
-                    break
-                }
-                else {
-                    flag = false
-                    ErrorStatement.text = "Password containing incorrect symbols. Please only use Letters, Numbers, Spaces, and the following symbols: '-', '_', '`, '."
-                }
-            }
-        }
-        
-        if flag == true {
-            //let jsonObject: NSDictionary = [
-            //    "name": name,
-            //    "username": username,
-            //    "password": password,
-            //    "email": email
-            //]
-            
-            //struct data {
-            //    let nam = name.self
-            //    let usernam = username
-            //    let passwor = password
-            //    let emai = email
-            //}
-            
-            struct jsonData: Codable {
-                let name: String
-                let username: String
-                let email: String
-                let password: String
-                let repeatPassword: String
-            }
-            
-            enum CodingKeys: String {
-                case name
-                case username
-                case email
-                case password
-            }
-            
-            let encoder = JSONEncoder()
-            
-            //let valid = JSONSerialization.isValidJSONObject(jsonData)
-        }
-        
-    }
-    
-    @IBAction func jumpToLogin(_ sender: LoginViewController) {
-        //jump to Login
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
+//
+//
+//import UIKit
+//
+//class SignUpViewController: UIViewController {
+//
+//    //String Inputs
+//    var name: String = ""
+//    var username: String = ""
+//    var email: String = ""
+//    var password: String = ""
+//    var repeatPassword: String = ""
+//
+//    //Array
+//    let symbols = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm0123456789-_`. "
+//
+////    //Used for outputing an error statement for user
+////    @IBOutlet var ErrorStatement: UILabel!
+////
+////    //Name input
+////    @IBOutlet var takeName: UITextField!
+////
+////    //Username input
+////    @IBOutlet var takeUsername: UITextField!
+////
+////    //Email input
+////    @IBOutlet var takeEmail: UITextField!
+////
+////    //Password and Password check input
+////    @IBOutlet var takePassword: UITextField!
+////
+////    @IBOutlet var takeRepeatPassword: UITextField!
+////
+////    //Check to make sure the inputs are correct
+////    @IBAction func Register(sender: ViewController) {
+////
+////        //Take Inputs
+////        name = takeName.text!
+////        username = takeUsername.text!
+////        email = takeEmail.text!
+////        password = takePassword.text!
+////        repeatPassword = takeRepeatPassword.text!
+////
+////        //Flag to see if username and password can be accepted
+////        var flag: Bool = true
+////
+////        //Check if passwords match
+////        if password != repeatPassword {
+////            flag = false
+////            ErrorStatement.text = "Passwords do not match"
+////        }
+////
+////        //Checks length of username
+////        if username.count < 8 {
+////            if username.count > 16 {
+////                flag = false
+////                ErrorStatement.text = "Username must be between 8 and 16 characters"
+////            }
+////        }
+////
+////        //Checks length of password
+////        if password.count < 8 {
+////            if password.count > 16 {
+////                flag = false
+////                ErrorStatement.text = "Password must be between 8 and 16 characters"
+////            }
+////        }
+////
+////        //Checks that username is the correct symbols
+////        for i in username {
+////
+////            for j in symbols {
+////                if i == j {
+////                    break
+////                }
+////                else {
+////                    flag = false
+////                    ErrorStatement.text = "Username containing incorrect symbols. Please only use Letters, Numbers, Spaces, and the following symbols: '-', '_', '`, '."
+////                }
+////            }
+////        }
+////
+////        //Checks that password is the correct symbols
+////        for i in password {
+////
+////            for j in symbols {
+////                if i == j {
+////                    break
+////                }
+////                else {
+////                    flag = false
+////                    ErrorStatement.text = "Password containing incorrect symbols. Please only use Letters, Numbers, Spaces, and the following symbols: '-', '_', '`, '."
+////                }
+////            }
+////        }
+////
+////        if flag == true {
+////            //let jsonObject: NSDictionary = [
+////            //    "name": name,
+////            //    "username": username,
+////            //    "password": password,
+////            //    "email": email
+////            //]
+////
+////            //struct data {
+////            //    let nam = name.self
+////            //    let usernam = username
+////            //    let passwor = password
+////            //    let emai = email
+////            //}
+////
+////            struct jsonData: Codable {
+////                let name: String
+////                let username: String
+////                let email: String
+////                let password: String
+////                let repeatPassword: String
+////            }
+////
+////            enum CodingKeys: String {
+////                case name
+////                case username
+////                case email
+////                case password
+////            }
+////
+////            let encoder = JSONEncoder()
+////
+////            //let valid = JSONSerialization.isValidJSONObject(jsonData)
+////        }
+////
+////    }
+////
+////    @IBAction func jumpToLogin(_ sender: LoginViewController) {
+////        //jump to Login
+////    }
+////
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//
+//    /*
+//     // MARK: - Navigation
+//
+//     // In a storyboard-based application, you will often want to do a little preparation before navigation
+//     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//     // Get the new view controller using segue.destinationViewController.
+//     // Pass the selected object to the new view controller.
+//     }
+//     */
+//
+//}
--- login/login/ViewController.swift
+++ login/login/ViewController.swift
@@ -1,13 +1,18 @@
 
-
+import Foundation
 import UIKit
 import CoreLocation
-import AWSCognitoIdentityProvider
+//import AWSCognitoIdentityProvider.h
 
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-        
+    @IBOutlet weak var webView: UIWebView!
+    
+   
+    
+    
+    
         //LocationManager base
         let locManager = CLLocationManager()
         
@@ -15,62 +20,68 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         var username: String = ""
         var password: String = ""
         
-        @IBOutlet var ErrorStatement: UILabel!
+       // @IBOutlet var ErrorStatement: UILabel!
         
-        @IBOutlet var takeUsername: UITextField!
+  
         
-        @IBOutlet var takePassword: UITextField!
+    @IBOutlet weak var takeUsername: UITextField!
+    
+    @IBOutlet weak var takePassword: UITextField!
+    
+    @IBAction func checkLogin(_ sender: ViewController) {
+    
         
-        @IBAction func checkLogin(_ sender: ViewController) {
-            //Take Inputs
-            username = takeUsername.text!
-            password = takePassword.text!
-            
-            //Flag to see if username and password are correct
-            var flag: Bool = false
-            
-            //Info from Json
-            struct StoredData {
-                var name: String
-                var username: String
-                var email: String
-                var password: String
-            }
-            
-            struct jsonData: Codable {
-                let name: String
-                let username: String
-                let email: String
-                let password: String
-            }
-            
-            let data = try! JSONSerialization.data(withJSONObject: StoredData.self, options: [])
-            let decoder = JSONDecoder()
-            let stored = try! decoder.decode(jsonData.self, from: data)
-            
-            if username == stored.username {
-                if password == stored.password {
-                    flag = true
-                }
-            }
-            
-            if flag == true {
-                //Successful login
-            }
-            else {
-                ErrorStatement.text = "Incorrect Username or Password"
-            }
-            
+        
+        
+//            //Take Inputs
+//            username = takeUsername.text!
+//            password = takePassword.text!
+//
+//            //Flag to see if username and password are correct
+//            var flag: Bool = false
+//
+//            //Info from Json
+//            struct StoredData {
+//                var name: String
+//                var username: String
+//                var email: String
+//                var password: String
+//            }
+//
+//            struct jsonData: Codable {
+//                let name: String
+//                let username: String
+//                let email: String
+//                let password: String
+//            }
+//
+//            let data = try! JSONSerialization.data(withJSONObject: StoredData.self, options: [])
+//            let decoder = JSONDecoder()
+//            let stored = try! decoder.decode(jsonData.self, from: data)
+//
+//            if username == stored.username {
+//                if password == stored.password {
+//                    flag = true
+//                }
+//            }
+//            
+//            if flag == true {
+//                //Successful login
+//            }
+//            else {
+//                ErrorStatement.text = "Incorrect Username or Password"
+//            }
+//
         }
         
-        @IBAction func jumpToSignUp(_ sender: SignUpViewController) {
-            //Jump to Signup
-        }
-        
-        @IBAction func jumpToGuest(_ sender: ViewController) {
-            //Jump to Camera for now
-        }
-        
+//        @IBAction func jumpToSignUp(_ sender: SignUpViewController) {
+//            //Jump to Signup
+//        }
+//
+//        @IBAction func jumpToGuest(_ sender: ViewController) {
+//            //Jump to Camera for now
+//        }
+    
         
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -79,8 +90,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locManager.requestWhenInUseAuthorization()
             locManager.requestAlwaysAuthorization()
             startReceivingLocation()
+            let requestURL = URL(string:"https://hywz-auth.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=7iea4023volnd3pfdp7jstdg61&redirect_uri=hygieawastezeroios://")
+            let request = URLRequest(url: requestURL!)
+             webView.loadRequest(request)
         }
-        
+    
+    func asynchronousURLPoll(completion: @escaping (Error?) -> Void) {
+        while !(webView.request?.url?.absoluteString.hasPrefix("hygieawastezeroios://"))! {
+            
+        }
+        completion(URLParse) // Or completion(SomeError.veryBadError)
+    }
+    
+    func URLParse() {
+    if let error = error {
+    print("Oops! Something went wrong...")
+    } else {
+   let toSearch = webView.request.URL.absoluteString.components(separatedBy: "#id_token=")[1].components(separatedBy: "&")[0]
+    
+    }
+    }
+    
         //// ALL OF THESE FUNCTIONS SHOULD BE USED WHEN LOCATION IS RETRIEVED ////
         func startReceivingLocation(){
             let authStatus = CLLocationManager.authorizationStatus()
