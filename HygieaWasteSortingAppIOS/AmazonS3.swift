9391c4816bdeabb1e9191df25f0ea92b63b950ff
Joshua Albin <jaalbin@asu.edu>
2018-10-13 19:41:51 -0700

    Task #131
    Editing S3 functions

--- HygieaWasteSortingAppIOS/AmazonS3.swift
+++ HygieaWasteSortingAppIOS/AmazonS3.swift
@@ -1,3 +1,5 @@
+//Amazon S3 functions
+
 func uploadData() {
     
     let data: Data = Data()
@@ -35,5 +37,39 @@ func uploadData() {
             //Need
         }
         return nil;
+    }
+}
+
+func downloadData() {
+    let expression = AWSS3TransferUtilityDownloadExpression()
+    expression.progressBlock = {(task, progress) in DispatchQueue.main.async(execute: {
+        //Wait until done
+    })
+    }
+    
+    var completionHandler: AWSS3TransferUtilityDownloadCompletionHandlerBlock?
+    completionHandler = { (task, URL, data, error) -> Void in
+        DispatchQueue.main.async(execute: {
+            ErrorStatement.text = "Error: Not able to complete AWS S3"
+        })
+    }
+    
+    let transferUtility = AWSS3TransferUtility.default()
+    transferUtility.downloadData(
+        fromBucket: "HygieaWasteSortingAppIOS",
+        //need
+        key: "???",
+        expression: expression,
+        completionHandler: completionHandler
+        ).continueWith {
+            (task) -> AnyObject! in if let error = task.error {
+                print("Error: \(error.localizedDescription)")
+            }
+            
+            if let _ = task.result {
+                //need
+                
+            }
+            return nil;
     }
 }
