8a77c076471b9403ffe000de268171eeda34d206
Joshua Albin <jaalbin@asu.edu>
2018-10-14 19:09:22 -0700

    Task #131
    Progress of transfer

--- HygieaWasteSortingAppIOS/AmazonS3.swift
+++ HygieaWasteSortingAppIOS/AmazonS3.swift
@@ -73,3 +73,48 @@ func downloadData() {
             return nil;
     }
 }
+
+let progressView: UIProgressView! = UIProgressView()
+progressView.progress = 0.0;
+
+let data = Data()
+
+let expression = AWSS3TransferUtilityUploadExpression()
+expression.progressBlock = {(task, progress) in DispatchQueue.main.async(execute: {
+    //Wait until done
+    progressView.progress = Float(progress.fractionCompleted)
+})
+}
+
+let completionHandler: AWSS3TransferUtilityUploadCompletionHandlerBlock = { (task, error) -> Void in DispatchQueue.main.async(execute: {
+    if let error = error {
+        NSLog("Failed with error: \(error)")
+    }
+    else if(self.progressView.progress != 1.0) {
+        NSLog("Error: Failed.")
+    } else {
+        NSLog("Success.")
+    }
+})
+}
+
+var refUploadTask: AWSS3TransferUtilityTask?
+let transferUtility = AWSS3TransferUtility.default()
+transferUtility.uploadData(data,
+    //need
+    key: "???",
+    //need
+    contentType: "???",
+   expression: expression,
+   completionHandler: completionHandler).continueWith { (task) -> AnyObject! in
+    if let error = task.error {
+        print("Error: \(error.localizedDescription)")
+    }
+    
+    if let uploadTask = task.result {
+        //need
+        refUploadTask = uploadTask
+    }
+    
+    return nil;
+}
