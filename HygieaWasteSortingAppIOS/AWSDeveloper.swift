b8441f6897fe3c465f09fe25620e97ddff3b4648
Joshua Albin <jaalbin@asu.edu>
2018-10-24 23:31:24 -0700

    Task #146
    Need to have this tested

--- HygieaWasteSortingAppIOS/AWSDeveloper.swift
+++ HygieaWasteSortingAppIOS/AWSDeveloper.swift
@@ -1,9 +1,5 @@
-//import info from button
-//import ???
-let type = String: ""
+let type = ""
 
-//input type
-//type = ???
 //function to be user or developer
 func role (type: String) -> Bool {
     let isUser = (type == "User")
