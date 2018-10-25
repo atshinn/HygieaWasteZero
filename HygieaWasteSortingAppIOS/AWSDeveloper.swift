9e7b461642262f0dead4b1cda4420af97b2076e1
Joshua Albin <jaalbin@asu.edu>
2018-10-24 23:27:52 -0700

    Task #146
    Moving over

---
+++ HygieaWasteSortingAppIOS/AWSDeveloper.swift
@@ -0,0 +1,12 @@
+//import info from button
+//import ???
+let type = String: ""
+
+//input type
+//type = ???
+//function to be user or developer
+func role (type: String) -> Bool {
+    let isUser = (type == "User")
+    
+    return isUser
+}
